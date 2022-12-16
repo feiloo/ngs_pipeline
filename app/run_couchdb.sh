a9639f43d41204af5ccc5c2434fc105bbbb7f7b8
feiloo <105425286+feiloo@users.noreply.github.com>
2022-12-16 11:43:21 +0100

    switch to non-fork miniwdl as rootless is now supported, and use podman

--- app/run_couchdb.sh
+++ app/run_couchdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti -e COUCHDB_USER=testuser -e COUCHDB_PASSWORD=testpsw --rm -p 8001:5984 apache/couchdb
+podman run -ti -e COUCHDB_USER=testuser -e COUCHDB_PASSWORD=testpsw --rm -p 8001:5984 docker.io/apache/couchdb
--- app/run_rabbitmq.sh
+++ app/run_rabbitmq.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run --rm \
+podman run --rm \
 	-p 5672:5672 \
 	--hostname my-rabbit \
 	--name some-rabbit \
 	-e RABBITMQ_DEFAULT_USER=testuser \
 	-e RABBITMQ_DEFAULT_PASS=testpsw \
-	rabbitmq:3-management
+	docker.io/rabbitmq:3-management
