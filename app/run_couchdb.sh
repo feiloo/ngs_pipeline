f4a3441fe548d50323dca9e5ab16c3a66e8aeeff
feiloo <105425286+feiloo@users.noreply.github.com>
2023-02-17 10:19:11 +0100

    switch couchdb container to use volumes

--- app/run_couchdb.sh
+++ app/run_couchdb.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-podman run -ti -e COUCHDB_USER=testuser -e COUCHDB_PASSWORD=testpsw --rm -p 5984:5984 docker.io/apache/couchdb
+podman run -ti \
+	--name ngs_couchdb \
+	--rm \
+	--mount type=volume,src=ngs_couchdb_volume,dst=/opt/couchdb/data \
+	-e COUCHDB_USER=testuser \
+	-e COUCHDB_PASSWORD=testpsw \
+	-p 5984:5984 \
+	docker.io/apache/couchdb
