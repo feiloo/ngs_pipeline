da5a6f3b2d7c786ab099ceebf80b17e2f77d314c
feiloo <105425286+feiloo@users.noreply.github.com>
2023-03-03 14:42:17 +0100

    make filemaker api table and layout variable

--- app/src/app/filemaker_api.py
+++ app/src/app/filemaker_api.py
@@ -7,7 +7,7 @@ from app.model import filemaker_examination_types
 
 
 class Filemaker:
-    def __init__(self, server, user, psw):
+    def __init__(self, server, user, psw, table_name=None, layout=None):
         self.server = server
         self.user = user
         self.psw = psw
@@ -16,7 +16,19 @@ class Filemaker:
         self.ssl_verify = False
 
         self.fm_baseurl = f"https://{self.server}/fmi/data/v1/databases"
-        self.session_url = f"{self.fm_baseurl}/molpatho_Leistungserfassung/sessions"
+
+        # use fixed table name and layout for simplicity
+        if table_name is None:
+            self.table_name = 'molpatho_Leistungserfassung'
+        else:
+            self.table_name = table_name
+
+        if layout is None:
+            self.layout = 'Leistungserfassung'
+        else:
+            self.layout=layout
+
+        self.session_url = f"{self.fm_baseurl}/{self.table_name}/sessions"
 
         self._token_ttl = datetime.timedelta(14*1/(24*60)) # in days, around 14 minutes
 
@@ -89,25 +101,27 @@ class Filemaker:
 
     def get_all_records(self, offset, limit=1000):
         ''' bulk gets record in creation order and paginated '''
-        url = f'{self.fm_baseurl}/molpatho_Leistungserfassung/layouts/Leistungserfassung/records?_limit={limit}&_offset={offset}'
+        url = f'{self.fm_baseurl}/{self.table_name}/layouts/{self.layout}/records?_limit={limit}&_offset={offset}'
         return self._get(url)
 
-    def find_records(self):
-        url = f'{self.fm_baseurl}/molpatho_Leistungserfassung/layouts/Leistungserfassung/_find'
+    def find_records(self, layout=None):
+        if layout is None:
+            layout='Leistungserfassung'
+        url = f'{self.fm_baseurl}/{self.table_name}/layouts/{self.layout}/_find'
         data = json.dumps({"query":[{"Zeitstempel":">=10/18/2022"}],
                     "limit":100
                     })
         return self._post(url, data)
 
     def find_mp_record(self, token, mp_number):
-        url = f'{self.fm_baseurl}/molpatho_Leistungserfassung/layouts/Leistungserfassung/_find'
+        url = f'{self.fm_baseurl}/{self.table_name}/layouts/{self.layout}/_find'
         data = json.dumps({"query":[{"Mol_NR":f"=={mp_number}"}],
                     "limit":10
                     })
         return self._post(url, data)
 
     def get_new_records(self, day, month, year, examination_types=filemaker_examination_types):
-        url = f'{self.fm_baseurl}/molpatho_Leistungserfassung/layouts/Leistungserfassung/_find'
+        url = f'{self.fm_baseurl}/{self.table_name}/layouts/{self.layout}/_find'
 
         data = json.dumps({"query":[
             {"Zeitstempel":f">={int(month)}/{(day)}/{(year)}", 
