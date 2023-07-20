73c2c4a5eba1ebdb904cc54ca81ab9426f97398f
feiloo <105425286+feiloo@users.noreply.github.com>
2023-07-20 17:39:05 +0200

    add stub for nextflow workflow backend

--- app/src/app/workflow_backends.py
+++ app/src/app/workflow_backends.py
@@ -6,15 +6,13 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
-
-def workflow_backend_execute_miniwdl(db, config, pipeline_run, is_aborted):
+def run_workflow_io(cmd, db, config, pipeline_run, is_aborted):
     ''' a function that runs a pipeline run on a workflow backend
     results and logs are saved onto the filesystem by the workflow backend
     but also ingested into the database
 
     this allows searching and viewing and editing the results
     '''
-    logger.debug(f'workflow backend starts executing {pipeline_run.id}')
 
     try:
         output_dir = config['workflow_output_dir']
@@ -25,23 +23,11 @@ def workflow_backend_execute_miniwdl(db, config, pipeline_run, is_aborted):
 
         with tempfile.TemporaryFile() as stde:
             with tempfile.TemporaryFile() as stdo:
-                clc_host = config['clc_host']
-                clc_user = config['clc_user']
-                clc_psw = config['clc_psw']
-
-                cmd = ['miniwdl', 'run', 
-                        '--env', f'CLC_HOST={clc_host}', 
-                        '--env', f"CLC_USER={clc_user}", 
-                        '--env', f'CLC_PSW={clc_psw}', 
-                        '--dir', output_dir, 
-                        pipeline_run.workflow
-                        ] + [f'files={i}' for i in pipeline_run.input_samples]
 
                 pipeline_proc = subprocess.Popen(
                         cmd,
-                        stdout=stde, #subprocess.PIPE, 
-                        stderr=stdo, #subprocess.PIPE
-                        #capture_output=True
+                        stdout=stde,
+                        stderr=stdo,
                         )
 
                 # update db entry every second when the process runs
@@ -84,6 +70,7 @@ def workflow_backend_execute_miniwdl(db, config, pipeline_run, is_aborted):
         pipeline_run = pipeline_run.from_dict(pipeline_document)
         pipeline_run = db.save_obj(pipeline_run)
 
+
 def workflow_backend_execute_noop(db, config, pipeline_run, is_aborted):
     pass
 
@@ -91,9 +78,36 @@ def workflow_backend_execute_clc(db, config, pipeline_run, is_aborted):
     pass
 
 def workflow_backend_execute_nextflow(db, config, pipeline_run, is_aborted):
-    pass
+    samplesheet_path = Path()
+    cmd = ['nextflow', 'run', 
+            '/opt/cio/variantinterpretation',
+            '-c', f'/opt/cio/variantinterpretation.conf', 
+            '--input',
+            'f{samplesheet_path}'
+            ]
+
+    run_workflow_io(cmd, db, config, pipeline_run, is_aborted)
+
+
+def workflow_backend_execute_miniwdl(db, config, pipeline_run, is_aborted):
+    clc_host = config['clc_host']
+    clc_user = config['clc_user']
+    clc_psw = config['clc_psw']
+
+    cmd = ['miniwdl', 'run', 
+            '--env', f'CLC_HOST={clc_host}', 
+            '--env', f"CLC_USER={clc_user}", 
+            '--env', f'CLC_PSW={clc_psw}', 
+            '--dir', output_dir, 
+            pipeline_run.workflow
+            ] + [f'files={i}' for i in pipeline_run.input_samples]
+
+    run_workflow_io(cmd, db, config, pipeline_run, is_aborted)
+
 
 def workflow_backend_execute(db, config, pipeline_run, is_aborted, backend):
+    logger.debug(f'workflow backend starts executing {pipeline_run.id}')
+
     if backend == 'noop':
         workflow_backend_execute_noop(db, config, pipeline_run, is_aborted)
     elif backend == 'clc':
