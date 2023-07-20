5134529b5ce6c26706b504e3df930920c6739dbd
feiloo <105425286+feiloo@users.noreply.github.com>
2023-07-20 17:24:51 +0200

    add cli tests without click testing, because its not thread safe

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+import signal
 
 import pycouchdb as couch
 import pytest
@@ -11,6 +12,7 @@ from app.ui import create_app
 
 from click.testing import CliRunner
 from app.config import CONFIG, Config
+
 
 @pytest.fixture()
 def cli_runner():
@@ -35,9 +37,36 @@ def test_pipeline_status(db, config):
 
 
 @pytest.mark.incremental
-class TestAppStart:
-    def test_init_db(self, config, cli_runner, couchdb_server):
-        result = cli_runner.invoke(main, ['--dev','init'])
-        assert result.exit_code == 0
+class TestAppCli:
+    def test_init_db_dev(self, config, couchdb_server):
+        # apparently the click tests runner is not thread safe, switching to subprocess
+        #result = cli_runner.invoke(main, ['--dev','init'])
+        proc = subprocess.run(['ngs_pipeline', '--dev', 'init'])
+        exit_code = proc.returncode
+        assert exit_code == 0
         assert 'ngs_app' in couchdb_server
         couchdb_server.delete('ngs_app')
+
+    def test_worker(self, config, db):
+        proc = subprocess.Popen(['ngs_pipeline', '--dev', 'worker'])
+        time.sleep(5)
+        proc.send_signal(signal.SIGINT)
+        proc.wait()
+        exit_code = proc.returncode
+        assert exit_code == 0
+
+    def test_run(self, config, db):
+        proc = subprocess.Popen(['ngs_pipeline', '--dev', 'run'])
+        time.sleep(5)
+        proc.send_signal(signal.SIGINT)
+        proc.wait()
+        exit_code = proc.returncode
+        assert exit_code == 0
+
+    def test_beat(self, config, db):
+        proc = subprocess.Popen(['ngs_pipeline', '--dev', 'beat'])
+        time.sleep(5)
+        proc.send_signal(signal.SIGINT)
+        proc.wait()
+        exit_code = proc.returncode
+        assert exit_code == 0
